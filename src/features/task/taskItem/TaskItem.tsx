43714415f04fbeff2dbdce910f29ef8a15083d5c
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-12 11:17:05 +0900

    モーダルの表示

--- src/features/task/taskItem/TaskItem.tsx
+++ src/features/task/taskItem/TaskItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Checkbox from "@material-ui/core/Checkbox";
+import Modal from "@material-ui/core/Modal";
 import EventNoteIcon from "@material-ui/icons/EventNote";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
@@ -11,6 +12,16 @@ interface PropTyeps {
 }
 
 const TaskItem: React.FC<PropTyeps> = ({ task }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={styles.root}>
       <div className={styles.tytle}>
@@ -23,10 +34,7 @@ const TaskItem: React.FC<PropTyeps> = ({ task }) => {
           onClick={() => console.log(`check ${task.id}`)}
           className={styles.checkbox}
         />
-        <button
-          onClick={() => console.log(`edit ${task.id}`)}
-          className={styles.edit_button}
-        >
+        <button onClick={handleOpen} className={styles.edit_button}>
           <EditIcon className={styles.icon} />
         </button>
         <button
@@ -36,6 +44,14 @@ const TaskItem: React.FC<PropTyeps> = ({ task }) => {
           <DeleteIcon className={styles.icon} />
         </button>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div>モーダル！！！</div>
+      </Modal>
     </div>
   );
 };
