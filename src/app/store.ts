81cfcca121268b664d3e02c11fe7312ffff2ea39
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-19 21:08:12 +0900

    dispatch関数の型注釈

--- src/app/store.ts
+++ src/app/store.ts
@@ -9,7 +9,6 @@ export const store = configureStore({
   },
 });
 
-export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
@@ -17,3 +16,6 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
+
+// dispatch関数の型注釈として使用するため。
+export type AppDispatch = typeof store.dispatch;
