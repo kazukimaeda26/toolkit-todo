bf51d6d10324ce7d4ba7c2ae146f5f1414c0a520
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-19 21:17:06 +0900

    fetchTasksを発火させる記述

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Header from "./components/header/Header";
 import styles from "./App.module.scss";
 import TaskForm from "./features/task/taskForm/TaskForm";
 import TaskList from "./features/task/taskList/TaskList";
+import { fetchTasks } from "./features/task/taskSlice";
+import { AppDispatch } from "./app/store";
 import { auth } from "./firebase";
 
 const App: React.FC = () => {
   console.log(auth);
+  const dispatch: AppDispatch = useDispatch();
+
+  // 空の配列を渡すのはレンダリングされた場合のみに発火させたいため。
+  useEffect(() => {
+    const getData = () => {
+      dispatch(fetchTasks());
+    };
+    getData();
+  }, []);
   return (
     <div className={styles.root}>
       <div className={styles.wrapper}>
