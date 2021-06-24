e138a05c1245bd197d70cde4e948d21d2a4e9aeb
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-24 13:38:48 +0900

    handleSignOut関数の作成

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,10 +4,24 @@ import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import * as H from "history";
+import { auth } from "../../firebase";
 
 import styles from "./Header.module.scss";
 
-const Header: React.FC = () => {
+interface PropTypes {
+  history: H.History;
+}
+
+const Header: React.FC<PropTypes> = ({ history }) => {
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+      history.push("user-auth");
+    } catch (err) {
+      alert(err.message);
+    }
+  };
   return (
     <div className={styles.root}>
       <AppBar position="static" className={styles.app_bar}>
