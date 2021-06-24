7a2cbe40386a84f82f5f513637d163c9086af597
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-24 13:38:02 +0900

    見た目の作成

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
@@ -14,6 +15,7 @@ const Header: React.FC = () => {
           <Typography variant="h6" className={styles.title}>
             redux tool kit
           </Typography>
+          <Button onClick={handleSignOut}>ログアウト</Button>
         </Toolbar>
       </AppBar>
     </div>
