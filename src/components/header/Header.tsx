38d1b1271c516af6a77b4cd1502d68f6eb24e842
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-27 13:36:30 +0900

    サインアウト後にはroot_pathへ遷移

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC<PropTypes> = ({ history }) => {
   const handleSignOut = async () => {
     try {
       await auth.signOut();
-      history.push("user-auth");
+      history.push("/");
     } catch (err) {
       alert(err.message);
     }
